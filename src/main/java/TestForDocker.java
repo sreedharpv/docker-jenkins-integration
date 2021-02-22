61d79a27b1a2f56a5510d802007d5b7f463c08c5
Sridhar <venkatasridhar.p@gmail.com>
2021-02-22 12:09:01 +0000

    Added docker file

--- src/main/java/TestForDocker.java
+++ src/main/java/TestForDocker.java
@@ -1,5 +1,5 @@
 public class TestForDocker {
-    public static void main(String args[]) {
-        System.out.println("Simple Test!!!");
+    public static void main(String[] args) {
+        System.out.println("Simple Test!!");
     }
 }
